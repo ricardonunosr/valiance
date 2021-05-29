eea836bec43376fcddc1caac8a16108b4fd20d17
ricardonunosr <ricardonunosr@gmail.com>
2021-05-29 21:30:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Valiance
+
+My poor attempt at making a game engine with OpenGL.
+It is named "Valiance" because i want to try to recreate Valorant with it.
+p.s: i love valorant <3
+
+##### Dependencies:
+- [Premake 5](https://github.com/premake/premake-core)
+- [Glad](https://github.com/Dav1dde/glad) (Version 4.6 Compatibility Profile) 
+- [GLFW](https://github.com/glfw/glfw) 
+- [ImGui](https://github.com/ocornut/imgui) 
+- [glm](https://github.com/g-truc/glm)
+
+## Getting Started
+
+Only Tested in Visual Studio 2019
+
+Start by cloning the repository with `git clone https://github.com/ricardonunosr/valiance`.
+
+Windows:
+
+Run the bat `WinGenerateProjects.bat` this will run premake and generate Solutions files for Visual Studio 2019.
+
+
+
+
---
+++ Valiance/src/EntryPoint.cpp
@@ -0,0 +1,88 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+
+glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+{
+	glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+	return Projection * View * Model;
+}
+
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+
+	float m_ClearColor[4] { 0.8f, 0.4f, 0.2f, 1.0f };
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(0, 0.5f);
+		glEnd();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Test");
+		ImGui::ColorEdit4("Clear Color", m_ClearColor);
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
